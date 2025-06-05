9411cccc1bf358b31dc48a4b23a5753e82f8adf8
Gabriela Rubio <gabyrubioa@gmail.com>
2025-06-05 10:45:11 -0600

    Redefine the first entity, User.

--- src/main/java/com/magabyzr/storemgv2/Storemgv2Application.java
+++ src/main/java/com/magabyzr/storemgv2/Storemgv2Application.java
@@ -1,10 +1,13 @@
 package com.magabyzr.storemgv2;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 
 public class Storemgv2Application {
     public static void main(String[] args) {
+        //ApplicationContext context = SpringApplication.run(Storemgv2Application.class, args);
+
 
     }
 }
