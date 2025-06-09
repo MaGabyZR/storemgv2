2a394df7e7c174368254ff7225115ef9abedd7dc
Gabriela Rubio <gabyrubioa@gmail.com>
2025-06-09 11:14:22 -0600

    Creating the Category and Product table in V5 migration in flyway. Defining the 1 - * relationship between the Category and Product new entities.

---
+++ src/main/java/com/magabyzr/storemgv2/entities/Category.java
@@ -0,0 +1,26 @@
+package com.magabyzr.storemgv2.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name= "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Byte id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products = new HashSet<>();
+
+}
