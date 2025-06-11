dcd80916311eec0edccc68c46b55afe47bd5f21a
Gabriela Rubio <gabyrubioa@gmail.com>
2025-06-11 11:16:58 -0600

    Derived Query methods in Product Repository.

--- src/main/java/com/magabyzr/storemgv2/repositories/ProductRepository.java
+++ src/main/java/com/magabyzr/storemgv2/repositories/ProductRepository.java
@@ -3,5 +3,25 @@ package com.magabyzr.storemgv2.repositories;
 import com.magabyzr.storemgv2.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+  //Derived Query Methods for strings, ex.:
+  List<Product> findByName(String name);
+  List<Product> findByNameLike(String name);
+  List<Product> findByNameNotLike(String name);
+  List<Product> findByNameContaining(String name);
+  List<Product> findByNameNotContaining(String name);
+  List<Product> findByNameEndingWith(String name);
+  List<Product> findByNameStartingWith(String name);
+
+  //Derived Query Methods for numbers, ex.:
+  List<Product> findByPrice(BigDecimal price);
+  List<Product> findByPriceGreaterThan(BigDecimal price);
+  List<Product> findByPriceGreaterThanEqual(BigDecimal price);
+  List<Product> findByPriceLessThan(BigDecimal price);
+  List<Product> findByPriceLessThanEqual(BigDecimal price);
+  List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
+
   }
